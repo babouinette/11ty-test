46ef0a7546c03bd19915a8b4f43f8b4870357a6f
babouinette <sebastien.roussel.vet@gmail.com>
2020-09-17 09:55:19 +0200

    fix

---
+++ _data/products.js
@@ -0,0 +1,5 @@
+module.exports = async function () {
+  return [{ "_id": "d6329922-19f5-4673-9a82-45f4ec419086", "title": "Product" },
+  { "_id": "d6329922-19f5-4673-9a82-45f4ec419086", "title": "Product" }
+  ]
+}
